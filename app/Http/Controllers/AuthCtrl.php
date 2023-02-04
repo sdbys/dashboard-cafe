63f5c000dd01bfd22eef092df6f956534629b1b2
sdbys <guccmvu@gmail.com>
2023-02-04 22:49:30 +0700

    Update AuthCtrl.php

--- app/Http/Controllers/AuthCtrl.php
+++ app/Http/Controllers/AuthCtrl.php
@@ -1,15 +1,15 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\User;
 use Illuminate\Http\Request;
-
-class authCtrl extends Controller
+use Illuminate\Support\Facades\Auth;
+use Illuminante\Support\Facades\Hash;
+class AuthCtrl extends Controller
 {
-    function login(){
+     function login(){
         return view('auth.login');
     }
-
     function cek_login(Request $req){
         // validasi
         $req -> validate(
@@ -23,7 +23,62 @@ class authCtrl extends Controller
             ]
 
         );
-        // cek login
+         // cek login
+        if(Auth::attempt(['email' => $req->email,'password' => $req->password])){
+           $req-> session()->regenerate();
+           return redirect('/'); //dasboard
+        }
+        // jika user password salah kembali ke form login
+        $mess = [
+          "type" => "danger",
+            "text" => "Maaf Username Atau Password salah !"
+        ];
+        return back()->with($mess);
+    }
+    function registrasi () {
+        return view ('auth.register');
+    }
+    function save_registrasi(Request $req){
+        $req ->validate(
+            [
+                "name" => "required|max:50",
+                "email" => "required",
+                "password" => "required|min:8"
+            ],
+            [
+                "name.required" => "Maaf Nama Harus Di isi",
+                "name.max" => "maaf nama maximal 50 karakter",
+                "email.required" => "maaf email harus di isi",
+                "password.required" => "maaf password harus di isi",
+                "password.min" => "password minimal 8 karakter"
+            ]
+        );
+        try {
+            // save
+            User::create([
+                "name" => $req->name,
+                "email" => $req ->email,
+                "password" => Hash::make($req->password),
+                "level" => "User",
+                "status" => 1
+            ]);
+            $mess = [
+                "type" => "succes",
+                "text"=> "Registrasi Berhasil Dilakukan"
+            ];
+        }catch (Exception $err) {
+          $mess = [
+            "type" => "danger",
+            "text" => "Registrasi Gagal"
+          ];
+        }
+        return redirect("auth/login")->with($mess);
+    }
+    function logout (Request $req){
+        Auth::logout();
+        $req->session()->invalidate();
+        $req->session()->regenerateToken();
+        return redirect('auth/login');
         //if(auth())
     }
 }
